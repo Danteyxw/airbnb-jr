e97c7ce9fd4577ffcef425162fbacc3df7de80a0
wengkhing <kheryee@live.com>
2015-08-28 10:56:21 +0800

    Remove username and name fields from create_table_users migration file

--- db/migrate/20150828094848_create_table_users.rb
+++ db/migrate/20150828094848_create_table_users.rb
@@ -1,10 +1,8 @@
 class CreateTableUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username, null: false
-      t.string :password, null: false
       t.string :email, null: false
-      t.string :name, null: false
+      t.string :password, null: false
     end
   end
 end
